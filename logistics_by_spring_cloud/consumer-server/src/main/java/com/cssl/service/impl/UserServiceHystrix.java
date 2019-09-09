7082906df75ef6b5c394212d1dce54661977c2df
tqy4411 <tqy4411@foxmail.com>
2019-09-09 19:18:01 +0800

    登录注册，后台显示（2），寄件  2019年9月9日 18点02分

--- logistics_by_spring_cloud/consumer-server/src/main/java/com/cssl/service/impl/UserServiceHystrix.java
+++
@@ -1,4 +0,0 @@
-package com.cssl.service.impl;
-
-public class UserServiceHystrix {
-}
--- logistics_by_spring_cloud/logistics-server/src/main/java/com/cssl/mailing/service/IExpress_userService.java
+++
@@ -1,8 +0,0 @@
-package com.cssl.mailing.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.cssl.entity.ExpressUser;
-
-public interface IExpress_userService extends IService<ExpressUser> {
-    int saveExpress_user(ExpressUser expressUser);
-}
